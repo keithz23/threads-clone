57045da07fffc8a0277a3f3fcfae696c3bc38c33
keithz <vuongtuankiet33@gmail.com>
2025-11-14 01:03:50 +0700

    feat: add toggle like api

--- threads-be/src/modules/likes/likes.controller.ts
+++ threads-be/src/modules/likes/likes.controller.ts
@@ -3,11 +3,11 @@ import { LikesService } from './likes.service';
 import { LikeDto } from './dto/create-like.dto';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
-@Controller('like-toggle')
+@Controller('likes')
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
 
-  @Post('like')
+  @Post('like-toggle')
   like(@CurrentUser('id') userId: string, @Body() likeDto: LikeDto) {
     return this.likesService.toggleLike(userId, likeDto);
   }
